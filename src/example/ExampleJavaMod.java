b849696b7310e9a2f98c11f867139ea5d4476ee9
FractalFolder <bodya.g2007@gmail.com>
2026-02-27 13:19:27 +0100

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -16,39 +16,21 @@ public class ExampleJavaMod extends Mod{
 
     public ExampleJavaMod(){
         Log.info("Loaded ExampleJavaMod constructor.");
-
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
-    }
-
+    
     @Override
     public void loadContent(){
-        // Создаем новый предмет "Кристалл Пустоты"
-        new Item("void-crystal") {{
-            localizedName = "Кристалл Пустоты";
-            description = "Очень редкий и нестабильный ресурс.";
-            color = Color.valueOf("8e44ad"); // Фиолетовый цвет
-            explosiveness = 0.5f; // Он немного взрывоопасен!
-            flammability = 0.1f;
-        }};
+        // Здесь ты создавал предметы
+    }
 
+    // Этот метод добавляет кнопку в главное меню
     @Override
     public void init(){
-    // Находим контейнер главного меню
-    ui.menuGroup.fill(t -> {
-        t.bottom().right(); // Ставим кнопку       вниз направо
-        t.button("Мой Мод", () -> {
-            ui.showInfo("Кнопка работает!");
-        }).size(150, 50); // Задаем размер
-    });
-}
+        // Находим контейнер главного меню
+        ui.menuGroup.fill(t -> {
+            t.bottom().right(); // Ставим кнопку вниз направо
+            t.button("Мой Мод", () -> {
+                ui.showInfo("Кнопка работает!");
+            }).size(150, 50); // Задаем размер
+        });
+    }
+} // !!! ВАЖНО: Эта скобка закрывает класс ExampleJavaMod
